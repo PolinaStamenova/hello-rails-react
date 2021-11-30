e74c8a2752e111195eb4015a4ed1446dd282064d
PolinaStamenova <polina.stamenova@gmail.com>
2021-11-30 10:39:17 +0200

    Create App component

---
+++ app/javascript/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HelloWorld from "./HelloWorld";
+
+class App extends React.Component {
+  render() {
+    return;
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/' render={() => "Home!"} />
+        <Route path='/hello' render={() => <HelloWorld greeting='Friend' />} />
+      </Switch>
+    </BrowserRouter>;
+  }
+}
+
+export default App;
