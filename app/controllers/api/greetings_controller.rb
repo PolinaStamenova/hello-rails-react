efcc75035eb857b756e1c9b679771c0f18d4f970
PolinaStamenova <polina.stamenova@gmail.com>
2021-12-01 11:41:52 +0200

    Remove comments

--- app/controllers/api/greetings_controller.rb
+++ app/controllers/api/greetings_controller.rb
@@ -1,12 +1,3 @@
-# module Api
-#   class GreetingsController < ::ApplicationController
-#     def index
-#       greeting = Greeting.order(Arel.sql('RANDOM()')).first
-#       render json: { message: greeting.message }
-#     end
-#   end
-# end
-
 module Api
   class GreetingsController < ::ApplicationController
     def index
@@ -15,10 +6,3 @@ module Api
     end
   end
 end
-
-# class Api::GreetingsController < ApplicationController
-#   def index
-#     @greeting = Greeting.order(Arel.sql('RANDOM()')).first
-#     render json: { greeting: @greeting.message }
-#   end
-# end
