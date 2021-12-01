1210a95669305d7d3dc176b78b43a70925d279cc
PolinaStamenova <polina.stamenova@gmail.com>
2021-12-01 10:42:06 +0200

    Remove unused files

--- app/javascript/redux/api.js
+++ app/javascript/redux/api.js
@@ -1,12 +1,3 @@
-// import axios from "axios";
-
-// const ROOT_PATH = "api";
-// const GREETINGS_PATH = `${ROOT_PATH}/greetings`;
-
-// export const getRandomMessage = () => {
-//   return axios.get(GREETINGS_PATH);
-// };
-
 export const getRandomMessage = async () => {
   const response = await fetch(`api/greetings`);
   return response.json();
--- app/javascript/redux/configureStore.js
+++ app/javascript/redux/configureStore.js
@@ -1,13 +1,3 @@
-// import { createStore, combineReducers, applyMiddleware } from "redux";
-// import thunk from "redux-thunk";
-// import greetingsReducer from "./greetings/index";
-
-// const reducer = combineReducers({ greetings: greetingsReducer });
-
-// const store = createStore(reducer, applyMiddleware(thunk));
-
-// export default store;
-
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import greetingsReducer from "./greetings";
